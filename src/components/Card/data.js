11bc88c66a207138d205d96f33fd53e1c4ee7c7f
Malkarsen <malko.arina212@gmail.com>
2025-07-30 18:00:37 +0200

    Homework_02 JSX_Components

---
+++ src/components/Card/data.js
@@ -0,0 +1,20 @@
+const user = {
+  avatar:
+    "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
+  fullName: "Homer Simpson",
+  activity: "Safety Inspector at the Springfield Nuclear Power Plant (Sector 7G)",
+  hobbies: ["Food", "Sofa lifestyle", "Cinema and TV"],
+};
+
+function getListOfHobbies(hobbies) {
+//   let list = "";
+//   for (let i = 0; i < hobbies.length; i++) {
+//     list.concat(`<li>${hobbies[i]}</li>`);
+//   }
+//   return list;
+//   return hobbies.map((hobby, index) => (
+//     <li key={index}>{hobby}</li>
+//   ));
+}
+
+export { user, getListOfHobbies };
